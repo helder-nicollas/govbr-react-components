a5c1a4953b0f556dd82b7d29490d329a862a53bf
Helder Nicollas Leão Peixoto <helder.peixoto@nees.ufal.br>
2025-02-19 18:11:53 -0300

    docs: improve textarea docs

--- src/components/textarea/textarea.stories.tsx
+++ src/components/textarea/textarea.stories.tsx
@@ -1,12 +1,21 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Textarea, TextareaProps } from '.';
 import { textareaVariants } from './variants';
+import { TextareaField } from './textarea-field';
 
 export default {
     title: 'Components/Textarea',
     component: Textarea,
+    subcomponents: {
+        TextareaField,
+    },
+    tags: ['autodocs'],
+} as Meta<TextareaProps>;
+
+export const Default: StoryObj<TextareaProps> = {
     args: {
         variant: 'normal',
+        size: 'medium',
     },
     argTypes: {
         variant: {
@@ -14,18 +23,16 @@ export default {
                 type: 'inline-radio',
             },
             options: Object.keys(textareaVariants.variants.variant),
+            description: 'Refere-se à variante a ser aplicada ao botão',
         },
         size: {
             control: {
                 type: 'inline-radio',
             },
             options: Object.keys(textareaVariants.variants.size),
+            description: 'Tamanho do textarea',
         },
     },
-    tags: ['autodocs'],
-} as Meta<TextareaProps>;
-
-export const Default: StoryObj<TextareaProps> = {
     render: args => {
         return (
             <Textarea {...args}>
