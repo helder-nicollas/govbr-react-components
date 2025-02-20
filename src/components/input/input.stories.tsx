a9252ca905decb40542e5ad55c57f8b36d9acb71
Helder Nicollas Leão Peixoto <helder.peixoto@nees.ufal.br>
2025-02-20 12:41:08 -0300

    docs: improve input docs with jsdocs

--- src/components/input/input.stories.tsx
+++ src/components/input/input.stories.tsx
@@ -1,10 +1,32 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { InputProps, Input } from '.';
 import { inputVariants } from './variants';
+import { InputField } from './Input-field';
+import { InputButton } from './input-button';
+import { InputGroup } from './input-group';
+import { InputIcon } from './input-icon';
+import { Label } from '../label';
 
+/***
+ * Renderiza um composto e flexível Input.
+ * Ele é formado por quatro outros componentes:
+ *
+ * - InputField: Tag **input**
+ * - InputButton: Botão do input
+ * - InputGroup: Agrupamento do input para utilizar com botões ou ícones
+ * - InputIcon: Ícone do input
+ *
+ *
+ */
 export default {
     title: 'Components/Input',
     component: Input,
+    subcomponents: {
+        InputField,
+        InputButton,
+        InputGroup,
+        InputIcon,
+    },
     id: 'Input',
     args: {
         variant: 'normal',
@@ -13,16 +35,24 @@ export default {
     },
     argTypes: {
         variant: {
+            description: 'Refere-se à variante que será aplicada no input.',
             control: {
                 type: 'inline-radio',
             },
             options: Object.keys(inputVariants.variants.variant),
         },
         size: {
+            description: 'Tamanho do input.',
             control: {
                 type: 'inline-radio',
             },
             options: Object.keys(inputVariants.variants.size),
+        },
+        highlight: {
+            description: 'Input com destaque.',
+        },
+        withButton: {
+            description: 'Deve ser marcada ao utilizar botão no input.',
         },
     },
     tags: ['autodocs'],
@@ -31,36 +61,59 @@ export default {
 export const Default: StoryObj<InputProps> = {
     render: args => {
         return (
-            <Input {...args}>
-                <Input.Field placeholder="Usuário" />
-            </Input>
-        );
-    },
-};
-export const WithButton: StoryObj<InputProps> = {
-    render: args => {
-        return (
-            <Input {...args} withButton>
-                <Input.Field placeholder="Usuário" />
-                <Input.Button>
-                    <i className="fas fa-eye" aria-hidden="true" />
-                </Input.Button>
-            </Input>
+            <div className="space-y-1">
+                <Label htmlFor="input">Nome de usuário:</Label>
+                <Input {...args}>
+                    <Input.Field placeholder="Ex. joaogovbr" id="input" />
+                </Input>
+            </div>
         );
     },
 };
 
-export const WithIcon: StoryObj<InputProps> = {
+/***
+ * Para utilizar o input com um botão, é necessário
+ * o botão e o component <code>InputField</code> estejam dentro do componente input group.
+ * Também é preciso que o componente <code>Input</code> esteja com a flag withButton marcada, conforme o exemplo abaixo:
+ */
+export const WithButton: StoryObj<InputProps> = {
+    name: 'Input com botão',
     render: args => {
         return (
-            <Input {...args}>
-                <Input.Group>
-                    <Input.Icon>
-                        <i className="fas fa-eye" aria-hidden="true" />
-                    </Input.Icon>
-                    <Input.Field placeholder="Usuário" />
-                </Input.Group>
-            </Input>
+            <div className="space-y-1">
+                <Label htmlFor="input">Nome de usuário:</Label>
+                <Input {...args} withButton>
+                    <InputGroup>
+                        <Input.Field placeholder="Ex. joaogovbr" id="input" />
+                        <Input.Button>
+                            <i className="fas fa-eye" aria-hidden="true" />
+                        </Input.Button>
+                    </InputGroup>
+                </Input>
+            </div>
+        );
+    },
+};
+
+/***
+ * Para utilizar o input com um ícone, é necessário
+ * o ícone e o component <code>InputField</code> estejam dentro do componente input group.
+ */
+export const WithIcon: StoryObj<InputProps> = {
+    name: 'Input com ícone',
+    render: args => {
+        return (
+            <div className="space-y-1">
+                <Label htmlFor="input">Nome de usuário:</Label>
+                <Input {...args}>
+                    <Input.Group>
+                        <Input.Icon>
+                            <i className="fas fa-eye" aria-hidden="true" />
+                        </Input.Icon>
+                        <Input.Field placeholder="Ex. joaogovbr" id="input" />
+                    </Input.Group>
+                </Input>
+            </div>
         );
     },
 };
