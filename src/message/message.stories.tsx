d9222b2f9c524fe8c44b4f4b6199a15fd94c01dc
Helder Nicollas Leão Peixoto <nicollashelder@gmail.com>
2025-03-07 16:01:04 -0300

    docs: improve message args docs

--- src/message/message.stories.tsx
+++ src/message/message.stories.tsx
@@ -6,6 +6,31 @@ import { fn } from '@storybook/test';
 export default {
     title: 'Components/Message',
     component: Message,
+    argTypes: {
+        icon: {
+            control: {
+                type: 'inline-radio',
+            },
+            options: Object.keys(messageVariants.variants.type),
+            description: 'Tipo do ícone.',
+        },
+        type: {
+            control: {
+                type: 'inline-radio',
+            },
+            options: Object.keys(iconVariants.variants.icon),
+            description: 'Tipo da mensagem.',
+        },
+        messageBody: {
+            description: 'Corpo da mensagem.',
+        },
+        messageTitle: {
+            description: 'Título da mensagem',
+        },
+        onClose: {
+            description: 'Função que será disparada ao fechar a mensagem',
+        },
+    },
     tags: ['autodocs'],
 } as Meta<typeof Message>;
 
@@ -17,19 +42,5 @@ export const Default: StoryObj<typeof Message> = {
             'Seus dados foram alterados conforme preenchimento do formulário.',
         messageTitle: 'Sucesso.',
         onClose: fn(),
-    },
-    argTypes: {
-        icon: {
-            control: {
-                type: 'inline-radio',
-            },
-            options: Object.keys(messageVariants.variants.type),
-        },
-        type: {
-            control: {
-                type: 'inline-radio',
-            },
-            options: Object.keys(iconVariants.variants.icon),
-        },
     },
 };
