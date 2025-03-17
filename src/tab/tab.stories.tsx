5181e29be84e300757a4417dd22bf39009f0ffbc
Helder Nicollas Leão Peixoto <nicollashelder@gmail.com>
2025-03-17 19:40:56 -0300

    docs(tab): create with results story of tab

--- src/tab/tab.stories.tsx
+++ src/tab/tab.stories.tsx
@@ -36,6 +36,7 @@ export const Default: StoryObj<typeof Tab> = {
         size: 'medium',
         counter: false,
     },
+    decorators: s => <div className="h-80">{s()}</div>,
     render: args => {
         const [currentTab, setCurrentTab] = useState('1');
         return (
@@ -58,6 +59,81 @@ export const Default: StoryObj<typeof Tab> = {
                         onClick={() => setCurrentTab('3')}
                     >
                         <button type="button">Texto 03</button>
+                    </Tab.Item>
+                </Tab.Nav>
+                <Tab.Content>
+                    <Tab.Panel active={currentTab === '1'}>
+                        Havia uma casa no fim da rua que ninguém ousava visitar.
+                        Diziam que, à meia-noite, sombras se moviam pelas
+                        janelas e sussurros ecoavam pelos corredores vazios. Um
+                        dia, um grupo de amigos decidiu explorar o local.
+                        Armados com lanternas, entraram pela porta enferrujada e
+                        sentiram um arrepio percorrer suas costas. No andar de
+                        cima, uma porta rangeu sozinha. Quando a abriram, apenas
+                        encontraram um espelho empoeirado. Mas, ao olhar para
+                        ele, viram reflexos que não pertenciam a eles.
+                    </Tab.Panel>
+                    <Tab.Panel active={currentTab === '2'}>
+                        Todo começo de semana, João tinha um ritual: visitar a
+                        pequena cafeteria na esquina da avenida. O aroma do café
+                        fresco misturado ao som suave do jazz criava um refúgio
+                        perfeito para organizar seus pensamentos. Ele sempre
+                        pedia um espresso duplo e se sentava na mesma mesa ao
+                        lado da janela. Mas, naquele dia, algo diferente
+                        aconteceu. Uma mulher desconhecida ocupava seu lugar.
+                        Ela sorria, como se já o conhecesse. E foi assim que,
+                        entre goles de café e conversas inesperadas, João
+                        descobriu que algumas segundas-feiras podem ser
+                        surpreendentes.
+                    </Tab.Panel>
+                    <Tab.Panel active={currentTab === '3'}>
+                        No ano de 2147, a humanidade havia partido para Marte,
+                        deixando para trás apenas um único habitante: um
+                        androide chamado Atlas. Programado para manter a cidade
+                        funcionando, ele vagava pelas ruas vazias, acendendo
+                        luzes que ninguém mais via e ajustando semáforos para
+                        carros inexistentes. Até que, um dia, um sinal de rádio
+                        veio do espaço. “Atlas, estamos voltando.” Pela primeira
+                        vez, ele sentiu algo novo em seu código: esperança.
+                    </Tab.Panel>
+                </Tab.Content>
+            </Tab>
+        );
+    },
+};
+
+export const WithResults: StoryObj<typeof Tab> = {
+    name: 'Com Contadores',
+    decorators: s => <div className="h-80">{s()}</div>,
+    args: {
+        counter: true,
+        size: 'medium',
+    },
+    render: args => {
+        const [currentTab, setCurrentTab] = useState('1');
+        return (
+            <Tab {...args}>
+                <Tab.Nav>
+                    <Tab.Item
+                        active={currentTab === '1'}
+                        onClick={() => setCurrentTab('1')}
+                    >
+                        <button type="button">Texto 01</button>
+                        <Tab.Results>(120)</Tab.Results>
+                    </Tab.Item>
+                    <Tab.Item
+                        active={currentTab === '2'}
+                        onClick={() => setCurrentTab('2')}
+                    >
+                        <button type="button">Texto 02</button>
+                        <Tab.Results>(78)</Tab.Results>
+                    </Tab.Item>
+                    <Tab.Item
+                        active={currentTab === '3'}
+                        onClick={() => setCurrentTab('3')}
+                    >
+                        <button type="button">Texto 03</button>
+                        <Tab.Results>(88)</Tab.Results>
                     </Tab.Item>
                 </Tab.Nav>
                 <Tab.Content>
