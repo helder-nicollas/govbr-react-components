d3b01dd54766b1facc189301408277e86ed71a9f
Helder Nicollas Leão Peixoto <nicollashelder@gmail.com>
2025-03-17 10:44:45 -0300

    fix(label): label styles as govbr-ds style

--- src/label/label.tsx
+++ src/label/label.tsx
@@ -1,5 +1,14 @@
 import { ComponentProps } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-export function Label({ children, ...props }: ComponentProps<'label'>) {
-    return <label {...props}>{children}</label>;
+export function Label({
+    children,
+    className,
+    ...props
+}: ComponentProps<'label'>) {
+    return (
+        <label {...props} className={twMerge('label', className)}>
+            {children}
+        </label>
+    );
 }
