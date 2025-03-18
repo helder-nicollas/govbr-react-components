d841d0c51d650a17ce89b89733f17d493d5000e8
Helder Nicollas Leão Peixoto <nicollashelder@gmail.com>
2025-03-18 10:22:34 -0300

    docs: improve button docs with all variants

--- src/button/button.stories.tsx
+++ src/button/button.stories.tsx
@@ -7,7 +7,7 @@ export default {
     title: 'Components/Button',
     component: Button,
     args: {
-        variant: 'primary',
+        variant: 'transparent',
         children: 'Button',
         loading: false,
         disabled: false,
@@ -56,10 +56,37 @@ export default {
     tags: ['autodocs'],
 } as Meta<ButtonProps>;
 
-export const Default: StoryObj = {
-    render: args => (
-        <Button as="a" {...args}>
-            Botão
-        </Button>
-    ),
+export const Default: StoryObj<typeof Button> = {
+    name: 'Default (Transparent)',
+    render: args => <Button {...args}>Botão</Button>,
+};
+
+export const Primary: StoryObj<typeof Button> = {
+    args: {
+        variant: 'primary',
+    },
+    render: args => <Button {...args}>Botão</Button>,
+};
+
+export const Outline: StoryObj<typeof Button> = {
+    args: {
+        variant: 'outline',
+    },
+    render: args => <Button {...args}>Botão</Button>,
+};
+
+export const Circle: StoryObj<typeof Button> = {
+    args: {
+        variant: 'primary',
+        circle: true,
+    },
+    render: args => <Button {...args}>B</Button>,
+};
+
+export const Loading: StoryObj<typeof Button> = {
+    args: {
+        variant: 'primary',
+        loading: true,
+    },
+    render: args => <Button {...args}>Botão</Button>,
 };
