2c5e656ecec697a7593c3aae98f4dfb7d04c43dd
Helder Nicollas Leão Peixoto <nicollashelder@gmail.com>
2025-03-18 19:27:27 -0300

    feat: remove icon variant from message component

--- src/feedback/variants.ts
+++ src/feedback/variants.ts
@@ -3,25 +3,25 @@ import { tv } from 'tailwind-variants';
 const feedbackVariants = tv({
     slots: {
         base: 'feedback',
-        icon: '',
+        icon: 'fas',
     },
     variants: {
         variant: {
             danger: {
                 base: 'danger',
-                icon: 'fas fa-times-circle',
+                icon: 'fa-times-circle',
             },
             success: {
                 base: 'success',
-                icon: 'fas fa-check-circle',
+                icon: 'fa-check-circle',
             },
             info: {
                 base: 'info',
-                icon: 'fas fa-info-circle',
+                icon: 'fa-info-circle',
             },
             warning: {
                 base: 'warning',
-                icon: 'fas fa-exclamation-triangle',
+                icon: 'fa-exclamation-triangle',
             },
         },
     },
--- src/message/message.stories.tsx
+++ src/message/message.stories.tsx
@@ -1,34 +1,30 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Message } from './message';
-import { iconVariants, messageVariants } from './variants';
+import { messageVariants } from './variants';
 import { fn } from '@storybook/test';
 
 export default {
     title: 'Components/Message',
     component: Message,
     argTypes: {
-        icon: {
+        variant: {
             control: {
                 type: 'inline-radio',
             },
-            options: Object.keys(messageVariants.variants.type),
+            options: Object.keys(messageVariants.variants.variant),
             description: 'Tipo do ícone.',
         },
-        type: {
-            control: {
-                type: 'inline-radio',
-            },
-            options: Object.keys(iconVariants.variants.icon),
-            description: 'Tipo da mensagem.',
+        ariaLabel: {
+            description: 'Atributo de acessibilidade para o componente.',
         },
         messageBody: {
             description: 'Corpo da mensagem.',
         },
         messageTitle: {
-            description: 'Título da mensagem',
+            description: 'Título da mensagem.',
         },
         onClose: {
-            description: 'Função que será disparada ao fechar a mensagem',
+            description: 'Função que será disparada ao fechar a mensagem.',
         },
     },
     tags: ['autodocs'],
@@ -36,8 +32,7 @@ export default {
 
 export const Default: StoryObj<typeof Message> = {
     args: {
-        type: 'success',
-        icon: 'success',
+        variant: 'success',
         messageBody:
             'Seus dados foram alterados conforme preenchimento do formulário.',
         messageTitle: 'Sucesso.',
